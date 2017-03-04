4740d0db7320f5f36de64231ff0efd6f7e92c5c5
ola <olamarcb@gmail.com>
2017-03-03 18:03:12 -0800

    Initial Commit

---
+++ Calculator1S/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator1S
+//
+//  Created by Ola Babalola on 3/3/17.
+//  Copyright © 2017 Ola Babalola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
