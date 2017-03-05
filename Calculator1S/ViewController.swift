ce8580d9346470cf8fa3783521da4cd0e0a7783d
ola <olamarcb@gmail.com>
2017-03-05 12:31:11 -0800

    Added π and sq rt features

--- Calculator1S/ViewController.swift
+++ Calculator1S/ViewController.swift
@@ -28,6 +28,20 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func performOperaton(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                display!.text = String(Double.pi)
+            case "√":
+                let operand = Double(display!.text!)!
+                display!.text = String(sqrt(operand))
+            default:
+                break
+            }
+        }
+    }
 
 }
 
